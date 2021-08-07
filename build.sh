d9e02467af5f03f8bb56d09ad3c2d8b5239ffedb
Sander Revenberg <sander@revenberg.info>
2021-08-07 17:35:50 +0200

    .

--- build.sh
+++ build.sh
@@ -3,9 +3,19 @@
 # version 2021-08-07 15:20
 
 cd ~/dockerp1logger
-rc=$(git remote show origin |  grep "local out of date" | wc -l)
 
-if [ $rc -ne "0" ]; then
+if [ -n "$1" ]; then
+  ex=$1
+else
+  rc=$(git remote show origin |  grep "local out of date" | wc -l)
+  if [ $rc -ne "0" ]; then
+    ex=true
+  else
+    ex=false
+  fi
+fi
+
+if [ $ex == true ]; then
     git pull
     chmod +x build.sh
 
